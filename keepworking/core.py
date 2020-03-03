0ae558ea31609aa471b90c5b4d60274efbf2d0b6
Lijing Ye <yelijing18@gmail.com>
2020-03-04 01:38:20 +0800

    fix: KeyboardInterrupt not handled in countdown

--- keepworking/core.py
+++ keepworking/core.py
@@ -10,22 +10,22 @@ pyautogui.PAUSE = 1
 
 
 def start():
-    print('请在倒计时内选中需要保持活动的窗口')
-    for i in range(5):
-        print(5 - i)
-        time.sleep(1)
-    print('移动鼠标到屏幕边角以退出程序')
-    while True:
-        try:
-            key = random.choice(['up', 'down'])
-            print('Press', key)
-            pyautogui.press(key)
-        except KeyboardInterrupt:
-            print('Quit by KeyboardInterrupt')
-            break
-        except FailSafeException:
-            print('Show confirm box')
-            if pyautogui.confirm(text='Are you sure you want to exit?', title='Confirm Exit',
-                                 buttons=['Keep Working!', 'Exit']) == 'Exit':
-                print('Quit by FailSafeCheck')
-                break
+    try:
+        print('请在倒计时内选中需要保持活动的窗口')
+        for i in range(5):
+            print(5 - i)
+            time.sleep(1)
+        print('移动鼠标到屏幕边角以退出程序')
+        while True:
+            try:
+                key = random.choice(['up', 'down'])
+                print('Press', key)
+                pyautogui.press(key)
+            except FailSafeException:
+                print('Show confirm box')
+                if pyautogui.confirm(text='Are you sure you want to exit?', title='Confirm Exit',
+                                     buttons=['Keep Working!', 'Exit']) == 'Exit':
+                    print('Quit by FailSafeCheck')
+                    break
+    except KeyboardInterrupt:
+        print('Quit by KeyboardInterrupt')
